880a507d51ebaafd8c908b7bccea399fef57b773
Waqas Ahmed Rehmani <waqasahmed.rehmani@uworx.co.uk>
2024-03-06 16:21:27 +0500

    Added navigation back to main page from button

--- DotnetTrainingStockApp/Views/StockItemDetailsPage.xaml.cs
+++ DotnetTrainingStockApp/Views/StockItemDetailsPage.xaml.cs
@@ -6,4 +6,9 @@ public partial class StockItemDetailsPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void OnAddToCartBtnClicked(object sender, EventArgs e)
+	{
+		await Shell.Current.GoToAsync("..");
+	}
 }
