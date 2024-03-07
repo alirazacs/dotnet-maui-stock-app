99f96d4da960bf63ef3f64dbbb71565188c926ec
Alamgir Hassan <alamgir.hassan@uworx.co.uk>
2024-03-07 15:05:12 +0500

    Run method on background thread

--- DotnetTrainingStockApp/Views/StockItemDetailsPage.xaml.cs
+++ DotnetTrainingStockApp/Views/StockItemDetailsPage.xaml.cs
@@ -11,6 +11,7 @@ namespace DotnetTrainingStockApp.Views;
 public partial class StockItemDetailsPage : ContentPage
 {
 	public string? Photo {  get; set; }
+    bool isBusy = false;
 	public StockItemDetailsPage(StockItemDetailsViewModel stockItemDetailsViewModel)
 	{
 		InitializeComponent();
@@ -24,13 +25,19 @@ public partial class StockItemDetailsPage : ContentPage
         ((StockItemDetailsViewModel)BindingContext).Path = Photo;
         if (Photo != null)
         {
-           var analyzeImage = AnalyzeImage(File.ReadAllBytes(Photo));
-           ((StockItemDetailsViewModel)BindingContext).Tags= analyzeImage.tags;
-           ((StockItemDetailsViewModel)BindingContext).ExpiryDate = analyzeImage.expiryDate;
+
+            Task.Run(async () =>
+            {
+                var analyzeImage = await AnalyzeImage(File.ReadAllBytes(Photo));
+                ((StockItemDetailsViewModel)BindingContext).Tags = analyzeImage.tags;
+                ((StockItemDetailsViewModel)BindingContext).ExpiryDate = analyzeImage.expiryDate;
+      
+            });
+
         }
     }
 
-    private AnalyzedImage AnalyzeImage(byte[] data)
+    private async Task<AnalyzedImage> AnalyzeImage(byte[] data)
     {
 
         //images used for scanning expiry date:
@@ -41,7 +48,6 @@ public partial class StockItemDetailsPage : ContentPage
         //https://prescriptionhope.com/wp-content/uploads/2020/01/Do-Medications-Expire.jpg
 
         BinaryData binaryData = new BinaryData(data);
-
         int maxTag = 5;
         string endpoint = "https://stockvision.cognitiveservices.azure.com/";
         string key = "209f0f88a4fd4522808428efe55be4c3";
@@ -49,13 +55,9 @@ public partial class StockItemDetailsPage : ContentPage
         List<string> tags = new List<string>();
         string expiryDate = null;
 
-        ImageAnalysisClient client = new ImageAnalysisClient(
-            new Uri(endpoint),
-            new AzureKeyCredential(key));
+        ImageAnalysisClient client = new ImageAnalysisClient(new Uri(endpoint), new AzureKeyCredential(key));
 
-        ImageAnalysisResult result = client.Analyze(
-            binaryData,
-            VisualFeatures.Tags | VisualFeatures.Read);
+        ImageAnalysisResult result = await client.AnalyzeAsync(binaryData, VisualFeatures.Tags | VisualFeatures.Read);
 
         string pattern = @"\b\d{1,2}/\d{1,2}/\d{2}(?:\d{2})?\b"; //for dates like : 12/01/21  OR 12/01/2021 (date/month/year)
 
@@ -76,44 +78,13 @@ public partial class StockItemDetailsPage : ContentPage
             }
         }
 
-        ListView listView = new ListView();
-        listView.SetBinding(ItemsView.ItemsSourceProperty, "tags");
+        // Other processing...
 
-        //reading the lines,words to fetch expiry date.
-
-        foreach (DetectedTextBlock block in result.Read.Blocks)
+        return new AnalyzedImage
         {
-            foreach (DetectedTextLine line in block.Lines)
-            {
-                foreach (DetectedTextWord word in line.Words)
-                {
-                    if (word.Text.Contains("/"))
-                    {
-                        //adding all dates having format with /
-                        dates.Add(word.Text);
-                    }
-                }
-            }
-        }
-
-        //get the expiry date....usually the expiry date is at last on most products so getting the last date...
-
-        if (dates.Count > 0)
-        {
-            Match match = Regex.Match(dates[dates.Count - 1], pattern);
-            if (match.Success)
-            {
-                expiryDate = dates[dates.Count - 1];
-            }
-        }
-
-        AnalyzedImage analyzeImage = new AnalyzedImage();
-        analyzeImage.expiryDate = expiryDate;
-        analyzeImage.tags = tags;
-        ExpiryDateLabel.Text = $"Expiry Date: {(String.IsNullOrEmpty(expiryDate) ? "Not Found" : expiryDate)}";
-        return analyzeImage;
-
-
+            expiryDate = expiryDate,
+            tags = tags
+        };
     }
 
     private async void OnAddToCartBtnClicked(object sender, EventArgs e)
